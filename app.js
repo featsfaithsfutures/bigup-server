31c92133d62d346d4e9bb57250f0d47b21fcee99
Kangwa Sambo <kangwasambo@gmail.com>
2014-11-16 15:04:56 -0500

    Updated package.json. Added sockets code for User and Channel bigups.

--- app.js
+++ app.js
@@ -1,52 +1,67 @@
 _           = require('underscore');
-azure       = require('azure')
+azure       = require('azure');
 fileSystem  = require('fs');
 path        = require('path');
-app         = require('express.io')()
-nconf       = require('nconf')
+app         = require('express.io')();
+nconf       = require('nconf');
 
-app.http().io()
-port = process.env.PORT || 2014
+app.http().io();
+port = process.env.PORT || 2014;
 
-app.io.route('bigup!', function(req) {
-    console.log("sending a big up to <" +req.data.userid+ ">");
-    bigUpUser(req.data.userid)
-})
+app.io.route('bigup/user', function(req) {
+    console.log("sending a big up to <" +req.data.recepientId+ ">");
+    bigUpUser(req.data.recepientId, req.data.userid);
+});
 
 app.io.route('login', function(req) {
   userid = req.data.userid;
   geotag = req.data.geo;
-  console.log("joining bigup channel room for tag <"+geotag+">")
-  req.io.join(getSubscriberOnlyRoom(geotag))
-})
+  console.log("joining bigup channel room for tag <"+geotag+">");
+  req.io.join(geotag);
+  console.log("joining dummy room for private big ups.");
+  req.io.join(req.data.userid);
+});
+
+app.io.route('bigup/channel', function(req) {
+  console.log("sending a big up to" < + req.data.geotag + ">");
+  bigUpGroup(req.data.geotag, req.data.userId);
+});
+
+function bigUpGroup(channelId, senderId) {
+  app.io.room(channelId).emit('bigup', {sender: senderId});
+}
+
+function bigUpUser (recepientId, senderId) {
+  app.io.room(userid).emit('bigup', {sender: senderId});
+}
 
 app.io.route('logout', function(req) {
-  console.log("no more big ups for <"+req.data.userid+">")
-  req.io.leave(getSubscriberOnlyRoom(req.data.geotag))
-})
+  console.log("no more big ups for <"+req.data.userid+">");
+  req.io.leave(req.data.geotag);
+});
 
 app.io.route('getBigUpInfo', function(req){
-  req.io.emit('bigupInfo', getBigUpInfoForUser(req.data.userid))
-})
+  req.io.emit('bigupInfo', getBigUpInfoForUser(req.data.userid));
+});
 
 // websocket route/event for getting leaderboard
 app.io.route('getLeaderBoard', function(req){
-  console.log("sending leaderboard")
-  req.io.emit('leaderBoard', _.first(getLeaderBoard(), 10))
-})
+  console.log("sending leaderboard");
+  req.io.emit('leaderBoard', _.first(getLeaderBoard(), 10));
+});
 
 app.get('/bigups/:userid', function(req, res){
-  res.json({bigupInfo: getBigUpInfoForUser(req.params.userid)})
-})
+  res.json({bigupInfo: getBigUpInfoForUser(req.params.userid)});
+});
 
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html')
-})
+  res.sendfile(__dirname + '/index.html');
+});
 
 
 app.get('/leaderboard', function(req, res) {
-    res.sendfile(__dirname + '/leaderboard.html')
-})
+  res.sendfile(__dirname + '/leaderboard.html');
+});
 
-app.listen(port)
+app.listen(port);
